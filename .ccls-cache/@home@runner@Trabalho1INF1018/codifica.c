7e935564bbd875b15daf7745afdeed02e32d72c5
João Marcello <131413084+jmal93@users.noreply.github.com>
2024-04-30 13:52:07 +0000

    correções menores do compacta

--- .ccls-cache/@home@runner@Trabalho1INF1018/codifica.c
+++ .ccls-cache/@home@runner@Trabalho1INF1018/codifica.c
@@ -1,104 +1,111 @@
+#include "codifica.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "codifica.h"
 
-char* intPraBin(unsigned int num, int size); //Passa de unsigned int pra uma string Binaria
-void escreveBitsNoFile(const char* bits, int numBits, FILE* file); //Pega a sequência de Bits e escreve no arquivo Binario
-void lebyte(FILE* file); // Faz o dump da sequência de bits do arquivo binario
+char *intPraBin(unsigned int num,
+                int size); // Passa de unsigned int pra uma string Binaria
+void escreveBitsNoFile(
+    const char *bits, int numBits,
+    FILE *file); // Pega a sequência de Bits e escreve no arquivo Binario
+void lebyte(FILE *file); // Faz o dump da sequência de bits do arquivo binario
 
 // Precisamos resolver o problema de tamanho (bin[100])...
-void compacta(FILE* arqTexto, FILE* arqBin, struct compactadora* v) {
-    char simbolo;
-    unsigned int codigo;
-    char bin[1000] = "";
-  
-    while (!feof(arqTexto)) {
-        simbolo = fgetc(arqTexto);
-        for (int i = 31; i >= 0; i--) {
-            if (simbolo == v[i].simbolo) {
-                char* codigoBin = intPraBin(v[i].codigo, v[i].tamanho);;
-                strcat(bin, codigoBin);
-                break;
-            }
-        }
-    }
+void compacta(FILE *arqTexto, FILE *arqBin, struct compactadora *v) {
+  int tamanhoBin = 0;
+  char simbolo;
+  char *bin = (char *)malloc(sizeof(char));
 
-    char* codigoBin = intPraBin(v[31].codigo, v[31].tamanho);
-    strcat(bin, codigoBin);
-    escreveBitsNoFile(bin, strlen(bin), arqBin);
+  while (!feof(arqTexto)) {
+    simbolo = fgetc(arqTexto);
+    for (int i = 31; i >= 0; i--) {
+      if (simbolo == v[i].simbolo) {
+        char *codigoBin = intPraBin(v[i].codigo, v[i].tamanho);
+        tamanhoBin += v[i].tamanho;
+        bin = (char *)realloc(bin, tamanhoBin * sizeof(char));
+        strcat(bin, codigoBin);
+        break;
+      }
+    }
+  }
+
+  char *codigoBin = intPraBin(v[31].codigo, v[31].tamanho);
+  tamanhoBin += v[31].tamanho;
+  bin = (char*) realloc(bin, tamanhoBin * sizeof(char));
+  strcat(bin, codigoBin);
+  printf("tamanho da string: %ld\ntamanho da variável: %d\n", strlen(bin),
+         tamanhoBin);
+  escreveBitsNoFile(bin, strlen(bin), arqBin);
+  free(bin);
 }
-void descompacta(FILE* arqBin, FILE* arqTexto, struct compactadora* v){
+void descompacta(FILE *arqBin, FILE *arqTexto, struct compactadora *v) {
   unsigned int byte;
   char bits[10000] = "";
   rewind(arqBin);
   while (!feof(arqBin)) {
-      fread(&byte, sizeof(unsigned char), 1, arqBin);
-      char *codigoBin = intPraBin(byte, 8);
-      strcat(bits, codigoBin);
-      if (byte == 0)
-          break;
+    fread(&byte, sizeof(unsigned char), 1, arqBin);
+    char *codigoBin = intPraBin(byte, 8);
+    strcat(bits, codigoBin);
   }
 
   char buffer[10000] = "";
-  for (int i = 0; i < strlen(bits); i++){
-          char bit = bits[i];
-          strcat(buffer, &bit);
-          for (int j = 0; j < 32; j++){
-              if (!strcmp(buffer, intPraBin(v[j].codigo, v[j].tamanho))){
-                  fputc(v[j].simbolo, arqTexto);
-                  strcpy(buffer, "\0");
-                  break;
-              }
-          }
+  for (int i = 0; i < strlen(bits); i++) {
+    char bit = bits[i];
+    strcat(buffer, &bit);
+    for (int j = 0; j < 32; j++) {
+      if (strcmp(buffer, intPraBin(v[j].codigo, v[j].tamanho)) == 0) {
+        fputc(v[j].simbolo, arqTexto);
+        strcpy(buffer, "\0");
+        break;
       }
-}
-
-
-void lebyte(FILE* file) { 
-    printf("Lebyte: \n");
-    unsigned char byte;
-    while (fread(&byte, sizeof(byte), 1, file)) {
-        printf("%02x ", byte);
     }
-    putchar('\n');
-}
-char* intPraBin(unsigned int num, int size) {
-  // Tamanho máximo do número binário para um unsigned int é 32 (com 32 bits)
-  char* bin = (char*)malloc((size + 1) * sizeof(char));
-  if (bin == NULL) {
-      fprintf(stderr, "Erro de alocação de memória\n");
-      exit(1);
   }
-  bin[size] = '\0'; // Terminate a string
+}
+
+void lebyte(FILE *file) {
+  printf("Lebyte: \n");
+  unsigned char byte;
+  while (fread(&byte, sizeof(byte), 1, file)) {
+    printf("%02x ", byte);
+  }
+  putchar('\n');
+}
+char *intPraBin(unsigned int num, int size) {
+  // Tamanho máximo do número binário para um unsigned int é 32 (com 32 bits)
+  char *bin = (char *)malloc((size) * sizeof(char));
+  if (bin == NULL) {
+    fprintf(stderr, "Erro de alocação de memória\n");
+    exit(1);
+  }
+  //bin[size] = '\0'; // Terminate a string
 
   // Loop para converter o número para binário
   for (int i = size - 1; i >= 0; --i) {
-      bin[i] = (num & 1) ? '1' : '0'; // Extrai o bit menos significativo
-      num >>= 1; // Desloca para a direita para verificar o próximo bit
+    bin[i] = (num & 1) ? '1' : '0'; // Extrai o bit menos significativo
+    num >>= 1; // Desloca para a direita para verificar o próximo bit
   }
 
   return bin;
 }
-void escreveBitsNoFile(const char* bits, int numBits, FILE* arq) {
+void escreveBitsNoFile(const char *bits, int numBits, FILE *arq) {
   int numBytes = numBits / 8;
   int resto = numBits % 8;
   int i, j;
   unsigned char byte = 0;
 
   for (i = 0; i < numBytes; i++) {
-      byte = 0;
-      for (j = 0; j < 8; j++) {
-          byte |= (bits[i * 8 + j] - '0') << (7 - j);
-      }
-      fwrite(&byte, sizeof(unsigned char), 1, arq);
+    byte = 0;
+    for (j = 0; j < 8; j++) {
+      byte |= (bits[i * 8 + j] - '0') << (7 - j);
+    }
+    fwrite(&byte, sizeof(unsigned char), 1, arq);
   }
 
   if (resto > 0) {
-      byte = 0;
-      for (i = 0; i < resto; i++) {
-          byte |= (bits[numBytes * 8 + i] - '0') << (7 - i);
-      }
-      fwrite(&byte, sizeof(unsigned char), 1, arq);
+    byte = 0;
+    for (i = 0; i < resto; i++) {
+      byte |= (bits[numBytes * 8 + i] - '0') << (7 - i);
+    }
+    fwrite(&byte, sizeof(unsigned char), 1, arq);
   }
 }
--- .ccls-cache/@home@runner@Trabalho1INF1018/main.c
+++ .ccls-cache/@home@runner@Trabalho1INF1018/main.c
@@ -3,31 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void) {
-  struct compactadora vCompactadora[32] = {
-      {'\n', 1, 8}, {'B', 1, 6},  {'D', 1, 5}, {'O', 16, 5}, {'M', 25, 5},
-      {'A', 5, 4},  {'I', 13, 4}, {0, 0, 0},   {0, 0, 0},    {0, 0, 0},
-      {0, 0, 0},    {0, 0, 0},    {0, 0, 0},   {0, 0, 0},    {0, 0, 0},
-      {0, 0, 0},    {0, 0, 0},    {0, 0, 0},   {0, 0, 0},    {0, 0, 0},
-      {0, 0, 0},    {0, 0, 0},    {0, 0, 0},   {0, 0, 0},    {0, 0, 0},
-      {0, 0, 0},    {0, 0, 0},    {0, 0, 0},   {0, 0, 0},    {0, 0, 0},
-      {0, 0, 0},    {4, 0, 12}};
-  FILE *entradaTexto = fopen("entradaTexto.txt", "r");
-  FILE *saidaTexto = fopen("saidaTexto.txt", "w");
-  FILE* Binario = fopen("Binario.bin", "rb+");
+int main(void)
+{
+	struct compactadora vCompactadora[32] = {
+		{'\n', 1, 8}, {'B', 1, 6}, {'D', 1, 5}, {'O', 16, 5}, {'M', 25, 5}, {'A', 5, 4}, {'I', 13, 4}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {4, 0, 12}};
+	FILE *entradaTexto = fopen("entradaTexto.txt", "r");
+	FILE *saidaTexto = fopen("saidaTexto.txt", "w");
+	FILE *Binario = fopen("Binario.bin", "rb+");
 
-  if(!entradaTexto || !saidaTexto || !Binario)
-  {
-    fprintf(stderr, "Erro ao abrir o arquivo\n");
-    exit(1);
-  }
+	if (!entradaTexto || !saidaTexto || !Binario)
+	{
+		fprintf(stderr, "Erro ao abrir o arquivo\n");
+		exit(1);
+	}
 
-  compacta(entradaTexto, Binario, vCompactadora);
-  descompacta(Binario, saidaTexto, vCompactadora);
+	compacta(entradaTexto, Binario, vCompactadora);
+	descompacta(Binario, saidaTexto, vCompactadora);
 
-  fclose(entradaTexto);
-  fclose(saidaTexto);
-  fclose(Binario);
-  printf("Fim do programa\n");
-  return 0;
+	fclose(entradaTexto);
+	fclose(saidaTexto);
+	fclose(Binario);
+	printf("Fim do programa\n");
+	return 0;
 }
