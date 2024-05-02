5d964950b5735c3f3c415ebc58bc94fccc53cb9a
João Marcello <131413084+jmal93@users.noreply.github.com>
2024-05-02 11:52:48 +0000

    agora escreve o EOT devidamente no binário e na descompacta pega os menores e maiores tamanhos dos caracteres

--- codifica.c
+++ codifica.c
@@ -1,158 +1,144 @@
+#include "codifica.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "codifica.h"
 
-char *intPraBin(unsigned int num, int size);                       // Passa de unsigned int pra uma string Binaria
-void escreveBitsNoFile(const char *bits, int numBits, FILE *file); // Pega a sequência de Bits e escreve no arquivo Binario
-void lebyte(FILE *file);                                           // Faz o dump da sequência de bits do arquivo binario
+char *intPraBin(unsigned int num,
+                int size); // Passa de unsigned int pra uma string Binaria
+void escreveBitsNoFile(
+    const char *bits, int numBits,
+    FILE *file); // Pega a sequência de Bits e escreve no arquivo Binario
+void lebyte(FILE *file); // Faz o dump da sequência de bits do arquivo binario
 
-void escreveByte(char *byte, FILE *arq)
-{
-    char c = 0x00;
-    for (int i = 0; i < 8; i++)
-    {
-        c = ((c << 1) | (byte[i] - '0'));
-    }
-    fwrite(&c, 1, 1, arq);
-    byte[0] = '\0';
+void escreveByte(char *byte, FILE *arq) {
+  char c = 0x00;
+  for (int i = 0; i < 8; i++) {
+    c = ((c << 1) | (byte[i] - '0'));
+  }
+  fwrite(&c, 1, 1, arq);
+  byte[0] = '\0';
 }
 
-char *completaByte(char *byte, char *resto)
-{
-    for (int i = strlen(byte); i < 8 && *resto; i++)
-    {
-        byte[i] = *resto;
-        resto++;
-    }
+char *completaByte(char *byte, char *resto) {
+  for (int i = strlen(byte); i < 8 && *resto; i++) {
+    byte[i] = *resto;
+    resto++;
+  }
 
-    return resto;
+  return resto;
 }
 
 // Precisamos resolver o problema de tamanho (bin[100])...
-void compacta(FILE *arqTexto, FILE *arqBin, struct compactadora *v)
-{
-    char simbolo;
-    char byte[9] = "", *codigoBin;
+void compacta(FILE *arqTexto, FILE *arqBin, struct compactadora *v) {
+  char simbolo;
+  char byte[9] = "", *codigoBin;
 
-    while (!feof(arqTexto))
-    {
-        simbolo = fgetc(arqTexto);
-        for (int i = 31; i >= 0; i--)
-        {
-            if (simbolo == v[i].simbolo)
-            {
-                codigoBin = intPraBin(v[i].codigo, v[i].tamanho);
-                while ((strlen(byte) + strlen(codigoBin)) > 8 && (strlen(byte) != 8))
-                {
-                    codigoBin = completaByte(byte, codigoBin);
-                    escreveByte(byte, arqBin);
-                }
-                strcat(byte, codigoBin);
-                if (strlen(byte) == 8)
-                {
-                    escreveByte(byte, arqBin);
-                }
-                break;
-            }
+  while (!feof(arqTexto)) {
+    simbolo = fgetc(arqTexto);
+    for (int i = 31; i >= 0; i--) {
+      if (simbolo == v[i].simbolo) {
+        codigoBin = intPraBin(v[i].codigo, v[i].tamanho);
+        while ((strlen(byte) + strlen(codigoBin)) > 8 && (strlen(byte) != 8)) {
+          codigoBin = completaByte(byte, codigoBin);
+          escreveByte(byte, arqBin);
         }
+        strcat(byte, codigoBin);
+        if (strlen(byte) == 8) {
+          escreveByte(byte, arqBin);
+        }
+        break;
+      }
     }
-    codigoBin = intPraBin(v[31].codigo, v[31].tamanho);
-    while ((strlen(byte) + strlen(codigoBin)) > 8 && (strlen(byte) != 8))
-    {
-        codigoBin = completaByte(byte, codigoBin);
-        escreveByte(byte, arqBin);
-    }
-    if (strlen(byte) <= 8)
-    {
-        escreveByte(byte, arqBin);
-    }
+  }
+  codigoBin = intPraBin(v[31].codigo, v[31].tamanho);
+  while ((strlen(byte) + strlen(codigoBin)) > 8 && (strlen(byte) != 8)) {
+    codigoBin = completaByte(byte, codigoBin);
+    escreveByte(byte, arqBin);
+  }
+  if (strlen(codigoBin) <= 8 && strlen(codigoBin) > 0) {
+    strcpy(byte, "00000000");
+    escreveByte(byte, arqBin);
+  }
 }
-void descompacta(FILE *arqBin, FILE *arqTexto, struct compactadora *v)
-{
-    unsigned int byte;
-    char bits[10000] = "";
-    rewind(arqBin);
-    while (!feof(arqBin))
-    {
-        fread(&byte, sizeof(unsigned char), 1, arqBin);
-        char *codigoBin = intPraBin(byte, 8);
-        strcat(bits, codigoBin);
-        if (byte == 0)
-            break;
-    }
+void descompacta(FILE *arqBin, FILE *arqTexto, struct compactadora *v) {
+  int maior = 0, menor = v[0].tamanho;
+  unsigned int byte;
+  char bits[10000] = "";
+  rewind(arqBin);
 
-    char buffer[10000] = "";
-    for (int i = 0; i < strlen(bits); i++)
-    {
-        char bit = bits[i];
-        strcat(buffer, &bit);
-        for (int j = 0; j < 32; j++)
-        {
-            if (!strcmp(buffer, intPraBin(v[j].codigo, v[j].tamanho)))
-            {
-                fputc(v[j].simbolo, arqTexto);
-                strcpy(buffer, "\0");
-                break;
-            }
-        }
+  for (int i = 0; i < 32; i++) {
+    if (v[i].tamanho > maior)
+      maior = v[i].tamanho;
+    if (v[i].tamanho < menor && v[i].tamanho != 0)
+      menor = v[i].tamanho;
+  }
+  while (!feof(arqBin)) {
+    fread(&byte, sizeof(unsigned char), 1, arqBin);
+    printf("%02x\n", byte);
+    char *codigoBin = intPraBin(byte, 8);
+    strcat(bits, codigoBin);
+    if (byte == 0)
+      break;
+  }
+
+  char buffer[10000] = "";
+  for (int i = 0; i < strlen(bits); i++) {
+    char bit = bits[i];
+    strcat(buffer, &bit);
+    for (int j = 0; j < 32; j++) {
+      if (!strcmp(buffer, intPraBin(v[j].codigo, v[j].tamanho))) {
+        fputc(v[j].simbolo, arqTexto);
+        strcpy(buffer, "\0");
+        break;
+      }
     }
+  }
 }
 
-void lebyte(FILE *file)
-{
-    printf("Lebyte: \n");
-    unsigned char byte;
-    while (fread(&byte, sizeof(byte), 1, file))
-    {
-        printf("%02x ", byte);
-    }
-    putchar('\n');
+void lebyte(FILE *file) {
+  printf("Lebyte: \n");
+  unsigned char byte;
+  while (fread(&byte, sizeof(byte), 1, file)) {
+    printf("%02x ", byte);
+  }
+  putchar('\n');
 }
-char *intPraBin(unsigned int num, int size)
-{
-    // Tamanho máximo do número binário para um unsigned int é 32 (com 32 bits)
-    char *bin = (char *)malloc((size + 1) * sizeof(char));
-    if (bin == NULL)
-    {
-        fprintf(stderr, "Erro de alocação de memória\n");
-        exit(1);
-    }
-    bin[size] = '\0'; // Terminate a string
+char *intPraBin(unsigned int num, int size) {
+  // Tamanho máximo do número binário para um unsigned int é 32 (com 32 bits)
+  char *bin = (char *)malloc((size + 1) * sizeof(char));
+  if (bin == NULL) {
+    fprintf(stderr, "Erro de alocação de memória\n");
+    exit(1);
+  }
+  bin[size] = '\0'; // Terminate a string
 
-    // Loop para converter o número para binário
-    for (int i = size - 1; i >= 0; --i)
-    {
-        bin[i] = (num & 1) ? '1' : '0'; // Extrai o bit menos significativo
-        num >>= 1;                      // Desloca para a direita para verificar o próximo bit
-    }
+  // Loop para converter o número para binário
+  for (int i = size - 1; i >= 0; --i) {
+    bin[i] = (num & 1) ? '1' : '0'; // Extrai o bit menos significativo
+    num >>= 1; // Desloca para a direita para verificar o próximo bit
+  }
 
-    return bin;
+  return bin;
 }
-void escreveBitsNoFile(const char *bits, int numBits, FILE *arq)
-{
-    int numBytes = numBits / 8;
-    int resto = numBits % 8;
-    int i, j;
-    unsigned char byte = 0;
+void escreveBitsNoFile(const char *bits, int numBits, FILE *arq) {
+  int numBytes = numBits / 8;
+  int resto = numBits % 8;
+  int i, j;
+  unsigned char byte = 0;
 
-    for (i = 0; i < numBytes; i++)
-    {
-        byte = 0;
-        for (j = 0; j < 8; j++)
-        {
-            byte |= (bits[i * 8 + j] - '0') << (7 - j);
-        }
-        fwrite(&byte, sizeof(unsigned char), 1, arq);
+  for (i = 0; i < numBytes; i++) {
+    byte = 0;
+    for (j = 0; j < 8; j++) {
+      byte |= (bits[i * 8 + j] - '0') << (7 - j);
     }
+    fwrite(&byte, sizeof(unsigned char), 1, arq);
+  }
 
-    if (resto > 0)
-    {
-        byte = 0;
-        for (i = 0; i < resto; i++)
-        {
-            byte |= (bits[numBytes * 8 + i] - '0') << (7 - i);
-        }
-        fwrite(&byte, sizeof(unsigned char), 1, arq);
+  if (resto > 0) {
+    byte = 0;
+    for (i = 0; i < resto; i++) {
+      byte |= (bits[numBytes * 8 + i] - '0') << (7 - i);
     }
+    fwrite(&byte, sizeof(unsigned char), 1, arq);
+  }
 }
